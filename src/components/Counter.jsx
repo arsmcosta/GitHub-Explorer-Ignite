173b1b4b7dbddb07d667694a90817cc43cfa9131
Renato Costa <arsmcosta@gmail.com>
2021-03-09 23:00:41 -0300

    Chapter I - Fast Refresh no Webpack

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -9,7 +9,7 @@ export function Counter() {
     return (
         <div>
             <h2>{counter}</h2>
-            <button type="button" onClick={increment}>Incremet</button>
+            <button type="button" onClick={increment}>Increment</button>
         </div>
     );
 }
